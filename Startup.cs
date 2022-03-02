b0cae2e55280a090b5c633f7eda4a7c0bab44202
Paul Mercer <visualmercer@googlemail.com>
2022-03-02 12:57:04 +0000

    Code tidy

--- Startup.cs
+++ Startup.cs
@@ -7,63 +7,62 @@ using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Models;
 using System.Diagnostics;
 
-namespace DotNetCoreSqlDb
+namespace DotNetCoreSqlDb;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var connString = Configuration.GetConnectionString("MyDbConnection");
+        Debug.WriteLine($"****************************** {connString} ***********************************");
+
+        services.AddControllersWithViews();
+        services.AddDbContext<MyDatabaseContext>(options =>
+                options.UseSqlServer(connString));
+    }
+
+    private static void UpdateDatabase(IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices
+            .GetRequiredService<IServiceScopeFactory>()
+            .CreateScope();
+        using var context = serviceScope.ServiceProvider.GetService<MyDatabaseContext>();
+        context.Database.Migrate();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        UpdateDatabase(app);
+
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //app.UseHsts();
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        //app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
         {
-            var connString = Configuration.GetConnectionString("MyDbConnection");
-            Debug.WriteLine($"****************************** {connString} ***********************************");
-
-            services.AddControllersWithViews();
-            services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlServer(connString));
-        }
-
-        private static void UpdateDatabase(IApplicationBuilder app)
-        {
-            using var serviceScope = app.ApplicationServices
-                .GetRequiredService<IServiceScopeFactory>()
-                .CreateScope();
-            using var context = serviceScope.ServiceProvider.GetService<MyDatabaseContext>();
-            context.Database.Migrate();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            UpdateDatabase(app);
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
-            }
-
-            //app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Todos}/{action=Index}/{id?}");
-            });
-        }
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Todos}/{action=Index}/{id?}");
+        });
     }
 }
