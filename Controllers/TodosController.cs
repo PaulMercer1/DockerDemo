5b53b49661142805b44b9bbf730a28f6190a0401
Paul Mercer <paul.mercer@qa.com>
2023-04-21 21:32:33 +0100

    Move to .net 7.0

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -1,4 +1,4 @@
-﻿using DotNetCoreSqlDb.Models;
+using DotNetCoreSqlDb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -8,138 +8,122 @@ namespace DotNetCoreSqlDb.Controllers;
 
 public class TodosController : Controller
 {
-    private readonly MyDatabaseContext _context;
+	private readonly MyDatabaseContext _context;
 
-    public TodosController(MyDatabaseContext context)
-    {
-        _context = context;
-    }
+	public TodosController(MyDatabaseContext context)
+	{
+		_context = context;
+	}
 
-    // GET: Todos
-    public async Task<IActionResult> Index()
-    {
-        return View(await _context.Todo.ToListAsync());
-    }
+	// GET: Todos
+	public async Task<IActionResult> Index()
+	{
+		return View(await _context.Todo.ToListAsync());
+	}
 
-    // GET: Todos/Details/5
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
+	// GET: Todos/Details/5
+	public async Task<IActionResult> Details(int? id)
+	{
+		return await ReturnViewForId(id);
+	}
 
-        var todo = await _context.Todo.FirstOrDefaultAsync(m => m.ID == id);
+	// GET: Todos/Create
+	public IActionResult Create()
+	{
+		return View(new Todo { CreatedDate = DateTime.Now });
+	}
 
-        if (todo == null)
-        {
-            return NotFound();
-        }
+	// POST: Todos/Create
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Create([Bind("ID,Description,CreatedDate")] Todo todo)
+	{
+		if (ModelState.IsValid)
+		{
+			_context.Add(todo);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+		return View(todo);
+	}
 
-        return View(todo);
-    }
+	// GET: Todos/Edit/5
+	public async Task<IActionResult> Edit(int? id)
+	{
+		if (id == null)
+		{
+			return NotFound();
+		}
 
-    // GET: Todos/Create
-    public IActionResult Create()
-    {
-        return View(new Todo { CreatedDate = DateTime.Now });
-    }
+		var todo = await _context.Todo.FindAsync(id);
+		return todo == null ? NotFound() : View(todo);
+	}
 
-    // POST: Todos/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("ID,Description,CreatedDate")] Todo todo)
-    {
-        if (ModelState.IsValid)
-        {
-            _context.Add(todo);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        return View(todo);
-    }
+	// POST: Todos/Edit/5
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Edit(int id, [Bind("ID,Description,CreatedDate")] Todo todo)
+	{
+		if (id != todo.ID)
+		{
+			return NotFound();
+		}
 
-    // GET: Todos/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
+		if (ModelState.IsValid)
+		{
+			try
+			{
+				_context.Update(todo);
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				var exists = await TodoExistsAsync(todo.ID);
+				if (!exists)
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+			return RedirectToAction(nameof(Index));
+		}
+		return View(todo);
+	}
 
-        var todo = await _context.Todo.FindAsync(id);
-        if (todo == null)
-        {
-            return NotFound();
-        }
-        return View(todo);
-    }
+	// GET: Todos/Delete/5
+	public async Task<IActionResult> Delete(int? id)
+	{
+		return await ReturnViewForId(id);
+	}
 
-    // POST: Todos/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("ID,Description,CreatedDate")] Todo todo)
-    {
-        if (id != todo.ID)
-        {
-            return NotFound();
-        }
+	private async Task<IActionResult> ReturnViewForId(int? id)
+	{
+		if (id == null)
+		{
+			return NotFound();
+		}
 
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _context.Update(todo);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                var exists = await TodoExistsAsync(todo.ID);
-                if (!exists)
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(todo);
-    }
+		var todo = await _context.Todo.FirstOrDefaultAsync(m => m.ID == id);
 
-    // GET: Todos/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
+		return todo == null ? NotFound() : View(todo);
+	}
 
-        var todo = await _context.Todo.FirstOrDefaultAsync(m => m.ID == id);
+	// POST: Todos/Delete/5
+	[HttpPost, ActionName("Delete")]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> DeleteConfirmed(int id)
+	{
+		var todo = await _context.Todo.FindAsync(id);
+		_context.Todo.Remove(todo);
+		await _context.SaveChangesAsync();
+		return RedirectToAction(nameof(Index));
+	}
 
-        if (todo == null)
-        {
-            return NotFound();
-        }
-
-        return View(todo);
-    }
-
-    // POST: Todos/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        var todo = await _context.Todo.FindAsync(id);
-        _context.Todo.Remove(todo);
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
-
-    private async Task<bool> TodoExistsAsync(int id)
-    {
-        return await _context.Todo.AnyAsync(e => e.ID == id);
-    }
+	private async Task<bool> TodoExistsAsync(int id)
+	{
+		return await _context.Todo.AnyAsync(e => e.ID == id);
+	}
 }
--- Startup.cs
+++ Startup.cs
@@ -64,10 +64,8 @@ public class Startup
         {
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //app.UseHsts();
         }
 
-        //app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseRouting();
         app.UseAuthorization();
